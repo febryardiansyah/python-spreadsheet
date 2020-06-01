1e8650ecae35423bc0e90b1bd8d90c9db44b5993
Bagus Bayu Sasongko <18104005@ittelkom-pwt.ac.id>
2020-06-01 20:24:14 +0700

    Update method Parse
    method parse  punya parameter index(integer) buat masukin array indeks ke berapa-nya

--- main.py
+++ main.py
@@ -9,29 +9,32 @@ class MainWindow(QWidget):
     def __init__(self):
         super().__init__()        
         self.app = QuickStart()
-        self.app.main()
+        #self.app.main()
         self.setup()
         self.parse()
     
-    def parse(self):
-        temp = ''
-        for row in self.app.main():
-            print(f'nilainya adalah :{row[0]}')
-            temp = row[0]
-        return temp
-    
+    def parse(self, index = 0):
+        message = self.app.main()
+        print(message)
+        for data in message:
+            print(f'nilainya adalah :{data}')
+        return message[index]
+
     def setup(self):
         print('this got rendered after spreadsheet')
         self.resize(400,300)
         self.move(300,300)
         self.setWindowTitle('Dame Dayo')
 
-        self.label1 = QLabel(f'{self.parse()}')
-
+        self.label1 = QLabel(f'{self.parse(0)}')
+        self.label2 = QLabel(f'{self.parse(1)}')
         layout = QGridLayout()
 
         layout.addWidget(self.label1,0,0)
+        layout.addWidget(self.label2,1,0)
         self.setLayout(layout)
+
+        
 
         
 
